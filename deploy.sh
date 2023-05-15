9c6301f0f253653482420d63ed4116c6df3b051d
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2023-05-15 12:31:38 +0200

    Update hosts file and switch from doom emacs to spacemacs

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ cwd=$(dirname $0)
 # System
 just -f /etc/justfile update
 just -f /etc/justfile setup-flatpaks
-#just -f /etc/justfile setup-dev-vscodium
-just -f /etc/justfile setup-dev-doom
+# just -f /etc/justfile setup-dev-vscodium
+# just -f /etc/justfile setup-dev-doom
+just -f /etc/justfile setup-dev-spacemacs
 just -f /etc/justfile setup-gnome-settings
 just -f /etc/justfile enable-services
 
