8eb7d21963ceb4edba0ddd9730ffb236785c3a12
magnouvean <c6af1d08-d039-472f-b890-6eaf2e96e961@anonaddy.me>
2023-05-22 23:56:45 +0200

    Update deploy script to add more options

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,32 @@
 set -e
 cwd=$(dirname $0)
 
-# System
-just -f /etc/justfile update
-just -f /etc/justfile setup-flatpaks
-# just -f /etc/justfile setup-dev-vscodium
-just -f /etc/justfile setup-dev-doom
-# just -f /etc/justfile setup-dev-spacemacs
-just -f /etc/justfile setup-gnome-settings
-just -f /etc/justfile enable-services
+if [ "$1" = "" ]; then
 
-# User
-just -f $cwd/justfile install-dotfiles
-just -f $cwd/justfile set-shell
-just -f $cwd/justfile user-gnome-settings
+    # System
+    just -f /etc/justfile update
+    just -f /etc/justfile setup-flatpaks
+    # just -f /etc/justfile setup-dev-vscodium
+    just -f /etc/justfile setup-dev-doom
+    # just -f /etc/justfile setup-dev-spacemacs
+    just -f /etc/justfile setup-gnome-settings
+    just -f /etc/justfile enable-services
+
+    # User
+    just -f $cwd/justfile install-dotfiles
+    just -f $cwd/justfile set-shell
+    just -f $cwd/justfile user-gnome-settings
+
+elif [ "$1" = "extra" ]; then
+
+    # Programming languages
+    just -f $cwd/justfile setup-dev-python
+    just -f $cwd/justfile setup-dev-julia
+    just -f $cwd/justfile setup-dev-R
+    just -f $cwd/justfile setup-dev-latex
+
+else
+
+    echo "Invalid argument: $1"
+
+fi
